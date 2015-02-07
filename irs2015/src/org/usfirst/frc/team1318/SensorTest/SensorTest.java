194a7d392069e6d85d5881f817b8820101a4f484
grclaeys <grclaeys@gmail.com>
2015-02-06 17:07:24 -0800

    Started work on the sensor tester

---
+++ irs2015/src/org/usfirst/frc/team1318/SensorTest/SensorTest.java
@@ -0,0 +1,94 @@
+package org.usfirst.frc.team1318.SensorTest;
+
+/**
+ * 
+ * @author grclaeys
+ * sensors that will be on the robot and need to be tested
+ * 
+ * One through beam sensor
+ * Two Ultrasonic sensors
+ * 3 Dip switches
+ * 3 encoders
+ * void Pressure Sensor
+ * Current Pressure Sensor
+ * 
+ *
+ */
+public class SensorTest
+{
+    // keys declarations   
+    private static final String THROUGH_BEAM_SENSOR_KEY = "";
+    private static final String ULTRASONIC_SENSOR_0_KEY = "";
+    private static final String ULTRASONIC_SENSOR_1_KEY = "";
+    private static final String DIP_SWITCH_0_KEY = "";
+    private static final String DIP_SWITCH_1_KEY = "";
+    private static final String DIP_SWITCH_2_KEY = "";
+    private static final String ENCODER_0_KEY = "";
+    private static final String ENCODER_1_KEY = "";
+    private static final String ENCODER_2_KEY = "";
+    private static final String BOOLEAN_PRESSURE_SENSOR = "";
+    private static final String CURRENT_PRESSURE_SENSOR = "";
+
+    //Checks the status of the through beam sensor
+    //    public void checkThroughBeamSensor()
+    //    {
+    //        SmartDashboardLogger.putBoolean(THROUGH_BEAM_SENSOR_KEY, value);
+    //    }
+    //
+    //    //Checks the status of the ultrasonic sensor
+
+    //    public void checkUltrasonicSensor0()
+    //    {
+    //        SmartDashboardLogger.putNumber(ULTRASONIC_SENSOR_0_KEY, value);
+    //    }
+    //
+    //    public void checkUltrasonicSensor1()
+    //    {
+    //        SmartDashboardLogger.putNumber(ULTRASONIC_SENSOR_1_KEY, value);
+    //    }
+    //
+    //    //Checks the status of the dipswitches
+    //    public void checkDipSwitch0()
+    //    {
+    //        SmartDashboardLogger.putBoolean(DIP_SWITCH_0_KEY, value);
+    //    }
+    //
+    //    public void checkDipSwitch1()
+    //    {
+    //        SmartDashboardLogger.putBoolean(DIP_SWITCH_1_KEY, value);
+    //    }
+    //
+    //    public void checkDipSwitch2()
+    //    {
+    //        SmartDashboardLogger.putBoolean(DIP_SWITCH_2_KEY, value);
+    //    }
+    //
+    //    //Checks the status of the encoders
+    //    public void checkEncoder0()
+    //    {
+    //        SmartDashboardLogger.putNumber(ENCODER_0_KEY, value);
+    //    }
+    //
+    //    public void checkEncoder1()
+    //    {
+    //        SmartDashboardLogger.putNumber(ENCODER_1_KEY, value);
+    //    }
+    //
+    //    public void checkEncoder2()
+    //    {
+    //        SmartDashboardLogger.putNumber(ENCODER_2_KEY, value);
+    //    }
+    //
+    //    //Checks the status of the boolean pressure sensor    
+    //    public void checkBooleanPressureSensor()
+    //    {
+    //        SmartDashboardLogger.putBoolean(BOOLEAN_PRESSURE_SENSOR, value);
+    //    }
+    //
+    //    //Checks the status of the regular pressure sensor
+    //    public void checkPressureSensor()
+    //    {
+    //        SmartDashboardLogger.putNumber(CURRENT_PRESSURE_SENSOR, value);
+    //    }
+
+}
